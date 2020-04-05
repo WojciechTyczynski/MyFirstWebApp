41304df8b97fedbae0c02310356a2aeff3c8a670
WojciechTyczynski <wojty98@gmail.com>
2020-04-05 15:21:16 +0200

    second commit

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def get_something():
+    return {"method": "GET"}
+
+@app.post("/method")
+def post_something():
+    return {"method":"POST"}
+
+@app.put("/method")
+def put_something():
+    return {"method":"PUT"}
+
+@app.delete("/method")
+def delete_something():
+    return {"method":"DELETE"}
